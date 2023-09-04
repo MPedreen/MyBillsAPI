196502a19c9b01e66275d65de9f0362206e87c66
Pedro Ferreira <pedrophpvp12@gmail.com>
2023-09-03 23:00:07 -0300

    feat: instalação mediator na camada de Application e criação da model ContaCreateCommand

---
+++ MyBills.Application/Models/Commands/ContaCreateCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using MyBills.Application.Models.Queries;
+using MyBills.Domain.Models.Enums;
+
+namespace MyBills.Application.Models.Commands
+{
+    public class ContaCreateCommand : IRequest<ContaQuery>
+    {
+        public string Numero { get; set; }
+        public decimal Valor { get; set; }
+        public TipoContaEnum Tipo { get; set; }
+        public bool EstaPaga { get; set; }
+        public DateTime DataVencimento { get; set; }
+    }
+}
