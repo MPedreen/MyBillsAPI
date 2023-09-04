86541d1c37ad56d7962d8f2fd9b5d6a6683a0b89
Pedro Ferreira <pedrophpvp12@gmail.com>
2023-09-03 22:30:04 -0300

    feat: criação model da Conta e criação do enum dos respectivos tipos

---
+++ MyBills.Domain/Models/Enums/TipoContaEnum.cs
@@ -0,0 +1,26 @@
+﻿namespace MyBills.Domain.Models.Enums
+{
+    public enum TipoContaEnum
+    {
+        Luz,
+        Agua,
+        Gas,
+        Internet,
+        Telefone,
+        IPTU,
+        IPVA,
+        Aluguel,
+        Condominio,
+        TVaCabo,
+        Celular,
+        SeguroResidencial,
+        PlanoDeSaude,
+        ServicosDeStreaming,
+        Educacao,
+        AssinaturaDeRevistasJornais,
+        TransportePublico,
+        LinhaDeCredito,
+        ImpostosDiversos,
+        Outros
+    }
+}
