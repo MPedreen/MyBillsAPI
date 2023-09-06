a46e08ba930fea23079fe1b4b16db065ccf24350
Pedro Ferreira <pedrophpvp12@gmail.com>
2023-09-05 22:15:38 -0300

    feat: definição de data annotations na model de criação da conta

--- MyBills.Application/Models/Commands/ContaCreateCommand.cs
+++ MyBills.Application/Models/Commands/ContaCreateCommand.cs
@@ -1,15 +1,27 @@
 ﻿using MediatR;
 using MyBills.Application.Models.Queries;
 using MyBills.Domain.Models.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace MyBills.Application.Models.Commands
 {
     public class ContaCreateCommand : IRequest<ContaQuery>
     {
+        [MinLength(6, ErrorMessage = "Informe no mínimo {1} caracteres.")]
+        [MaxLength(20, ErrorMessage = "Informe no máximo {1} caracteres.")]
+        [Required(ErrorMessage = "Campo obrigatório.")]
         public string Numero { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
         public decimal Valor { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
         public TipoContaEnum Tipo { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
         public bool EstaPaga { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
         public DateTime DataVencimento { get; set; }
     }
 }
