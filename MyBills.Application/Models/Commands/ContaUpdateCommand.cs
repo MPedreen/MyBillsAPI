27380ecb1623d59059885ebb80a72ee2e678093b
Pedro Ferreira <pedrophpvp12@gmail.com>
2023-09-05 22:35:12 -0300

    feat: criação create, update e delete command da conta

---
+++ MyBills.Application/Models/Commands/ContaUpdateCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using MyBills.Application.Models.Queries;
+
+namespace MyBills.Application.Models.Commands
+{
+    public class ContaUpdateCommand : IRequest<ContaQuery>
+    {
+
+    }
+}
--- MyBills.Application/Services/ContaAppService.cs
+++ MyBills.Application/Services/ContaAppService.cs
@@ -1,7 +1,43 @@
-﻿namespace MyBills.Application.Services
+﻿using MediatR;
+using MyBills.Application.Models.Commands;
+using MyBills.Application.Models.Queries;
+
+namespace MyBills.Application.Services
 {
     public class ContaAppService
     {
+        private readonly IMediator? _mediator;
 
+        public ContaAppService(IMediator? mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task<ContaQuery> Create(ContaCreateCommand command)
+        {
+            return await _mediator.Send(command);
+        }
+
+        public async Task<ContaQuery> Update(ContaUpdateCommand command)
+        {
+            return await _mediator.Send(command);
+        }
+
+        public async Task<ContaQuery> Delete(ContaDeleteCommand command)
+        {
+            return await _mediator.Send(command);
+        }
+
+        // TODO
+        //public List<ContaQuery> GetAll()
+        //{
+        //    return _produtoPersistence.GetAll();
+        //}
+
+        // TODO
+        //public ContaQuery GetById(Guid? id)
+        //{
+        //    return _produtoPersistence.GetById(id.Value);
+        //}
     }
 }
