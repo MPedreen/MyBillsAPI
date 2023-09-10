ca9596a92412ddc4cc9c8aa3b37fc8a2abf2c628
Pedro Ferreira <pedrophpvp12@gmail.com>
2023-09-10 17:47:45 -0300

    feat: criação Extension para registrar injeção de dependência e registro ContaAppService

---
+++ MyBillsAPI/Extensions/ServicesExtension.cs
@@ -0,0 +1,15 @@
+﻿using MyBills.Application.Interfaces.Services;
+using MyBills.Application.Services;
+
+namespace MyBills.API.Extensions
+{
+    public static class ServicesExtension
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddTransient<IContaAppService, ContaAppService>();
+
+            return services;
+        }
+    }
+}
