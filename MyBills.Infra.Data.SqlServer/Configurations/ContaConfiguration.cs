26a02661ff05c86368f47b48bbe48cebadddac5a
Pedro Ferreira <pedrophpvp12@gmail.com>
2023-09-10 19:11:37 -0300

    feat: criação context

---
+++ MyBills.Infra.Data.SqlServer/Configurations/ContaConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyBills.Domain.Models;
+
+namespace MyBills.Infra.Data.SqlServer.Configurations
+{
+    public class ContaConfiguration : IEntityTypeConfiguration<Conta>
+    {
+        public void Configure(EntityTypeBuilder<Conta> builder)
+        {
+
+        }
+    }
+}
