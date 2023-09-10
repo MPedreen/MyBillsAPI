98e78c7c73299e06c8060aab9dc79b0082b94229
Pedro Ferreira <pedrophpvp12@gmail.com>
2023-09-10 20:35:33 -0300

    feat: criação IUnitOfWork com registro das suas respectivas props herdadas

--- MyBillsAPI/Extensions/ServicesExtension.cs
+++ MyBillsAPI/Extensions/ServicesExtension.cs
@@ -1,5 +1,7 @@
 ﻿using MyBills.Application.Interfaces.Services;
 using MyBills.Application.Services;
+using MyBills.Domain.Interfaces.Repositories;
+using MyBills.Infra.Data.SqlServer.Repositories;
 
 namespace MyBills.API.Extensions
 {
@@ -8,6 +10,7 @@ namespace MyBills.API.Extensions
         public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddTransient<IContaAppService, ContaAppService>();
+            services.AddTransient<IUnitOfWork, UnitOfWork>();
 
             return services;
         }
