5ee7236fa8da52bf94eb5d3362c16819139b393c
Pedro Ferreira <pedrophpvp12@gmail.com>
2023-09-10 19:40:39 -0300

    feat: criação BaseRepository e seus respectivos métodos CRUD

---
+++ MyBills.Infra.Data.SqlServer/Repositories/BaseRepository.cs
@@ -0,0 +1,39 @@
+﻿using MyBills.Infra.Data.SqlServer.Contexts;
+
+namespace MyBills.Infra.Data.SqlServer.Repositories
+{
+    public abstract class BaseRepository<TModel, TKey>
+    {
+        private readonly DataContext _dataContext;
+
+        protected BaseRepository(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public virtual void Add(TModel model)
+            => _dataContext.Add(model);
+
+
+        public virtual void Update(TModel model)
+            => _dataContext.Update(model);
+
+        public virtual void Delete(TModel model)
+            => _dataContext.Remove(model);
+
+        public virtual List<TModel> GetAll()
+            => _dataContext.Set<TModel>().ToList();
+
+        public virtual List<TModel> GetAll(Func<TModel, bool> where)
+            => _dataContext?.Set<TModel>().Where(where).ToList();
+
+        public virtual TModel? GetById(TKey id)
+            => _dataContext?.Set<TModel>().Find(id);
+
+        public virtual TModel? Get(Func<TModel, bool> where)
+            => _dataContext?.Set<TModel>().FirstOrDefault(where);
+
+        public void Dispose()
+            => _dataContext?.Dispose();
+    }
+}
