cb000d73a11ff8997c33522253b70b5decbb95b2
Pedro Ferreira <pedrophpvp12@gmail.com>
2023-09-10 20:02:50 -0300

    feat: criação ContaRepository

---
+++ MyBills.Infra.Data.SqlServer/Repositories/ContaRepository.cs
@@ -0,0 +1,17 @@
+﻿using MyBills.Domain.Interfaces.Repositories;
+using MyBills.Domain.Models;
+using MyBills.Infra.Data.SqlServer.Contexts;
+
+namespace MyBills.Infra.Data.SqlServer.Repositories
+{
+    public class ContaRepository : BaseRepository<Conta, Guid>, IContaRepository
+    {
+        private readonly DataContext _dataContext;
+
+        public ContaRepository(DataContext dataContext)
+            : base(dataContext)
+        {
+            _dataContext = dataContext;
+        }
+    }
+}
