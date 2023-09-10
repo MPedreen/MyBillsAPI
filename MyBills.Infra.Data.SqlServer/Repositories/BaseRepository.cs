7135da3a060cbe4a7d561488ec01d08aafca81ec
Pedro Ferreira <pedrophpvp12@gmail.com>
2023-09-10 19:54:11 -0300

    feat: criação métodos CRUD IBaseRepository

--- MyBills.Infra.Data.SqlServer/Repositories/BaseRepository.cs
+++ MyBills.Infra.Data.SqlServer/Repositories/BaseRepository.cs
@@ -1,8 +1,10 @@
-﻿using MyBills.Infra.Data.SqlServer.Contexts;
+﻿using MyBills.Domain.Interfaces.Repositories;
+using MyBills.Infra.Data.SqlServer.Contexts;
 
 namespace MyBills.Infra.Data.SqlServer.Repositories
 {
-    public abstract class BaseRepository<TModel, TKey>
+    public abstract class BaseRepository<TModel, TKey> : IBaseRepository
+        where TModel : class
     {
         private readonly DataContext _dataContext;
 
@@ -14,7 +16,6 @@ namespace MyBills.Infra.Data.SqlServer.Repositories
         public virtual void Add(TModel model)
             => _dataContext.Add(model);
 
-
         public virtual void Update(TModel model)
             => _dataContext.Update(model);
 
@@ -25,15 +26,15 @@ namespace MyBills.Infra.Data.SqlServer.Repositories
             => _dataContext.Set<TModel>().ToList();
 
         public virtual List<TModel> GetAll(Func<TModel, bool> where)
-            => _dataContext?.Set<TModel>().Where(where).ToList();
+            => _dataContext.Set<TModel>().Where(where).ToList();
 
-        public virtual TModel? GetById(TKey id)
-            => _dataContext?.Set<TModel>().Find(id);
+        public virtual TModel GetById(TKey id)
+            => _dataContext.Set<TModel>().Find(id);
 
-        public virtual TModel? Get(Func<TModel, bool> where)
-            => _dataContext?.Set<TModel>().FirstOrDefault(where);
+        public virtual TModel Get(Func<TModel, bool> where)
+            => _dataContext.Set<TModel>().FirstOrDefault(where);
 
         public void Dispose()
-            => _dataContext?.Dispose();
+            => _dataContext.Dispose();
     }
 }
