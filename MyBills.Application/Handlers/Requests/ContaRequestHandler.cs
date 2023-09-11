b609472580695af03e3c832fd04f51a3483b4ff9
Pedro Ferreira <pedrophpvp12@gmail.com>
2023-09-11 20:18:02 -0300

    feat: ciação handlers conta

---
+++ MyBills.Application/Handlers/Requests/ContaRequestHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using MyBills.Application.Models.Commands;
+using MyBills.Application.Models.Queries;
+
+namespace MyBills.Application.Handlers.Requests
+{
+    public class ContaRequestHandler :
+        IRequestHandler<ContaCreateCommand, ContaQuery>,
+        IRequestHandler<ContaUpdateCommand, ContaQuery>,
+        IRequestHandler<ContaDeleteCommand, ContaQuery>
+    {
+        public Task<ContaQuery> Handle(ContaCreateCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ContaQuery> Handle(ContaDeleteCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ContaQuery> Handle(ContaUpdateCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
