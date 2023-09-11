e42e210ef383e2d1f5c492bf2baa66c9a04009b1
Pedro Ferreira <pedrophpvp12@gmail.com>
2023-09-11 20:28:41 -0300

    feat: implementando interface ContaDomainService herdando de IContaDomainService

--- MyBills.Domain/Services/ContaDomainService.cs
+++ MyBills.Domain/Services/ContaDomainService.cs
@@ -1,7 +1,46 @@
-﻿namespace MyBills.Domain.Services
+﻿using MyBills.Domain.Interfaces.Repositories;
+using MyBills.Domain.Interfaces.Services;
+using MyBills.Domain.Models;
+
+namespace MyBills.Domain.Services
 {
     public class ContaDomainService : IContaDomainService
     {
+        private readonly IUnitOfWork _unitOfWork;
 
+        public ContaDomainService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public void Add(Conta model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Conta model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Conta> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Conta GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Conta model)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
